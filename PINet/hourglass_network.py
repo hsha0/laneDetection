61064b91eb9a3732d2f5c9b820da1f738ef25326
Han Shao <hshao@oberlin.edu>
2020-12-19 14:19:11 -0500

    test epoch36

--- PINet/hourglass_network.py
+++ PINet/hourglass_network.py
@@ -22,7 +22,7 @@ class lane_detection_network(nn.Module):
         self.layer1 = hourglass_block(128, 128)
         self.layer2 = hourglass_block(128, 128)
         self.layer3 = hourglass_block(128, 128)
-        self.layer4 = hourglass_block(128, 128)
+        # self.layer4 = hourglass_block(128, 128)
 
 
     def forward(self, inputs):
@@ -31,7 +31,7 @@ class lane_detection_network(nn.Module):
         result1, out, feature1 = self.layer1(out)
         result2, out, feature2 = self.layer2(out)   
         result3, out, feature3 = self.layer3(out)
-        result4, out, feature4 = self.layer4(out)
+        # result4, out, feature4 = self.layer4(out)
 
-        return [result1, result2, result3, result4], [feature1, feature2, feature3, feature4]
+        return [result1, result2, result3], [feature1, feature2, feature3]
         #return [result1], [feature1]
